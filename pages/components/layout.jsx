a1efa38bd04a2ae3303635afbbdbcd099e8040e7
Stuart McNab <contactstuartonline@gmail.com>
2022-06-30 21:25:59 +0100

    start building components

---
+++ pages/components/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './nav';
+import Footer from './footer';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Nav />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Head from 'next/head';
+import Button from '@mui/material/Button';
+import styles from '../styles/Home.module.css';
+import Layout from './components/layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <Head>
+          <title>Stuart McNab - Frontend Web Developer</title>
+          <meta name="description" content="The official portfolio website for Stuart McNab, a front end web developer working with React.js, JavaScript, TypeScript, AWS and more to bring the best experience to users of web apps" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+        </Head>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Stuart McNab
+          </h1>
+          <h3>Frontend Web Developer</h3>
+          <Button variant="contained">This is an MUI button</Button>
+          <p className={styles.description}>
+            Get started by editing
+            {' '}
+            <code className={styles.code}>pages/index.js</code>
+          </p>
+
+          <div className={styles.grid}>
+            <a href="https://nextjs.org/docs" className={styles.card}>
+              <h2>Documentation &rarr;</h2>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
+
+            <a href="https://nextjs.org/learn" className={styles.card}>
+              <h2>Learn &rarr;</h2>
+              <p>Learn about Next.js in an interactive course with quizzes!</p>
+            </a>
+
+            <a
+              href="https://github.com/vercel/next.js/tree/canary/examples"
+              className={styles.card}
+            >
+              <h2>Examples &rarr;</h2>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
+
+            <a
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className={styles.card}
+            >
+              <h2>Deploy &rarr;</h2>
+              <p>
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </p>
+            </a>
+          </div>
+        </main>
+      </div>
+    </Layout>
+  );
+}
